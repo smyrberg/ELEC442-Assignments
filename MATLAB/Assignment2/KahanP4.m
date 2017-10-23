18f5b4b316d9ade41f9019ce1095ee69ce635f94
Sondre <sondre.myrberg@gmail.com>
2017-10-22 18:03:54 -0700

    Working on with assignment 2

---
+++ MATLAB/Assignment2/KahanP4.m
@@ -0,0 +1,7 @@
+function theta = KahanP4(a,b,c)
+    if a+b>c && c>abs(a-b)
+        theta = 2*atan(((a+b)^2 -c^2)/(c^2 - (a-b)^2));
+    else
+        theta = 'No solution';
+    end
+end
--- MATLAB/Assignment2/assignment2.m
+++ MATLAB/Assignment2/assignment2.m
@@ -17,5 +17,20 @@ o4_home = [431.8+433.07;149.09;20.32];
 o4 = od - 60*k6;
 
 %% Finding theta1
-theta1_home = KahanP2(k0, i0, o4_home-o0);
-theta1 = KahanP2(k0, i0, o4-o0);
+ux = sqrt(o4(1)^2 + o4(2)^2 - 149.09^2);
+uy = 149.09;
+uz = o4(3);
+u = [ux; uy; uz];
+
+theta1 = KahanP2(k0, u, o4);
+
+%% theta3
+a_home = 431.8;
+b_home = sqrt(433.07^2 + 20.32^2);
+c_home = sqrt((433.07+431.8)^2 + 20.30^2);
+theta3_offset = KahanP4(a_home, b_home, c_home);
+
+c = sqrt(o4(1)^2 + o4(3)^2); %Projected onto k0-i0 plane
+theta3 = KahanP4(a_home, b_home, c) - theta3_offset;
+
+%% theta2
